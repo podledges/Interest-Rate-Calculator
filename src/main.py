f47c72e33c54f58413cb3b6486aaf2d6d69f20a7
podles <podlevan@gmail.com>
2026-05-21 03:21:10 +0800

    make code more readable

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 import datetime
 
-# --- 1. Import your quantitative tools ---
-# Adjust the 'src.quant' or 'src.engine' paths if you moved files around!
+
+
 from src.quant.day_counter import calculate_year_fraction
 from src.quant.discount_factor import calculate_discount_factor, calculate_df_from_dates
-from src.engine.schedule import generate_payment_schedule # (Assuming you put this in engine)
+
+
 
 def main():
     print("=== SWAP CALCULATOR QUICK-TEST SANDBOX ===\n")
     
+
+    
     # ---------------------------------------------------------
-    #                 CONFIGURE ME
+    #                       CONFIGURE ME
     # ---------------------------------------------------------
     d_start = datetime.date(1990, 5, 17)
     d_end = datetime.date(1990, 6, 17)
@@ -20,16 +23,18 @@ def main():
     test_rate = 0.094765
     test_convention = "ACT/360"
     
+
     
     # ---------------------------------------------------------
-    # DAY COUNTER
+    #                       FRACTIONAL DAY COUNT
+    # PARAMS: date Start, date End, BASIS
     # ---------------------------------------------------------
     # fraction = calculate_year_fraction(d_start, d_end, test_convention)
     # print(f"[Day Counter] Year Fraction: {fraction:.6f}")
     
     
     # ---------------------------------------------------------
-    # DISCOUNT FACTOR                               [standalone]
+    #                       DISCOUNT FACTOR     [standalone]                              
     # PARAMS: RATE, TIME FRACTION
     # ---------------------------------------------------------
     # pure_df = calculate_discount_factor(rate=test_rate, time_fraction=0.086111)
@@ -37,11 +42,11 @@ def main():
     
     
     # ---------------------------------------------------------
-    # DISCOUNT FACTOR                               [integrated]
+    #                       DISCOUNT FACTOR     [integrated]
     # PARAMS: RATE, date Start, date End, Convention
     # ---------------------------------------------------------
-    integrated_df = calculate_df_from_dates(test_rate, d_start, d_end, test_convention)
-    print(f"[Integrated DF] Discount Factor: {integrated_df:.6f}")
+    # integrated_df = calculate_df_from_dates(test_rate, d_start, d_end, test_convention)
+    # print(f"[Integrated DF] Discount Factor: {integrated_df:.6f}")
     
 
 
